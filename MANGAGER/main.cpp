aff5c071cdf524b6ded95237bfd8bc2eba57e02f
Taro Verschueren <57659923+MrTV0@users.noreply.github.com>
2023-01-20 11:04:41 +0100

    Add files via upload

--- MANGAGER/main.cpp
+++ MANGAGER/main.cpp
@@ -5,6 +5,8 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <WiFiClientSecure.h>
+#include <WiFiUdp.h>
+#include "NTPClient.h"
 #include "Cipher.h"
 #include "SPIFFSTest.h"
 
@@ -48,15 +50,33 @@ String id_sensor5;
 String sensor5;
 String sensor6;
 String sensor7;
-const char *host = "hooyberghsapi20230117150416.azurewebsites.net";
+String banaan;
+const char* host = "hooyberghsapi20230117150416.azurewebsites.net"; // only google.com not https://google.com
 
-String banaan = "{\"pumpModelID\":1,\"position\":\"Nieuwe pomp\",\"wharfID\":43}";
+// String query_string = "{ "
+//                       "pumpModelID"
+//                       " : 1, "
+//                       "position"
+//                       " : "
+//                       "schriktor"
+//                       ", "
+//                       "wharfID"
+//                       " : 1}";
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-Cipher *cipher = new Cipher();
+Cipher * cipher = new Cipher();
 
 CSPIFFS mSpiffs;
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+// Variables to save date and time
+String formattedDate;
+String dayStamp;
+String timeStamp;
 
 void setup()
 {
@@ -111,10 +131,19 @@ void setup()
   LoRa.setSyncWord(0xF3);
   Serial.println("LoRa Initializing OK!");
 
-  char *key = "encrypted&secure";
+  char * key = "encrypted&secure";
   cipher->setKey(key);
   SPIFFS.begin(true);
   mSpiffs.listDir(SPIFFS, "/", 0);
+
+  // Initialize a NTPClient to get time
+  timeClient.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(3600);
 }
 
 void loop()
@@ -156,6 +185,89 @@ void loop()
     lcd.print("next");
   }
 
+  int packetSize = LoRa.parsePacket();
+  if (packetSize)
+  {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available())
+    {
+      String packetEncrypted = LoRa.readString();
+      mSpiffs.writeFile(SPIFFS, "/encrypted.txt", packetEncrypted);
+      String LoRaData = cipher->decryptString(mSpiffs.getFile(SPIFFS, "/encrypted.txt"));
+      LoRaData = LoRaData.substring(0, LoRaData.indexOf(0x2400));
+      Serial.print(LoRaData);
+
+      for (int i = 0; i < 12; i++)
+      {
+
+        separator = LoRaData.indexOf('|');
+        switch (i)
+        {
+        case 0:
+          id_esp32 = LoRaData.substring(0, separator);
+          break;
+        case 1:
+          id_sensor1 = LoRaData.substring(0, separator);
+          break;
+        case 2:
+          sensor1 = LoRaData.substring(0, separator);
+          break;
+        case 3:
+          id_sensor2 = LoRaData.substring(0, separator);
+          break;
+        case 4:
+          sensor2 = LoRaData.substring(0, separator);
+          break;
+        case 5:
+          sensor3 = LoRaData.substring(0, separator);
+          while(!timeClient.update()) {
+            timeClient.forceUpdate();
+          }
+          // The formattedDate comes with the following format:
+          // 2018-05-28T16:00:13Z
+          // We need to extract date and time
+          Serial.print("Datum:");
+          formattedDate = timeClient.getFormattedDate();
+          Serial.println(formattedDate);
+
+          banaan = "{\"timestamp\":\"";
+          banaan = banaan + formattedDate;
+          banaan = banaan + "\",\"value\":";
+          banaan = banaan + sensor3;
+          banaan = banaan + ",\"wharfID\":43}";
+          Serial.println(banaan);
+          
+          break;
+        case 6:
+          id_sensor4 = LoRaData.substring(0, separator);
+          break;
+        case 7:
+          sensor4 = LoRaData.substring(0, separator);
+          break;
+        case 8:
+          id_sensor5 = LoRaData.substring(0, separator);
+          break;
+        case 9:
+          sensor5 = LoRaData.substring(0, separator);
+          break;
+        case 10:
+          sensor6 = LoRaData.substring(0, separator);
+          break;
+        case 11:
+          sensor7 = LoRaData.substring(0, separator);
+          break;
+
+        default:
+          break;
+        }
+        LoRaData = LoRaData.substring(separator + 1, LoRaData.length());
+      }
+    }
+  }
+
   if (last_button3_state == HIGH && button3_state == LOW)
   {
     Serial.println("button Relay is pressed");
@@ -173,116 +285,46 @@ void loop()
     // Use WiFiClient class to create TCP connections
     WiFiClientSecure client;
     const int httpPort = 443; // 80 is for HTTP / 443 is for HTTPS!
-
+    
     client.setInsecure(); // this is the magical line that makes everything work
-
-    if (!client.connect(host, httpPort))
-    { // works!
+    
+    if (!client.connect(host, httpPort)) { //works!
       Serial.println("connection failed");
       return;
     }
 
     // We now create a URI for the request
-    String url = "/api/Pump/addPump";
+    String url = "/api/FlowRate/addFlowRate";
 
     Serial.print("Requesting URL: ");
     Serial.println(url);
 
     // This will send the request to the server
     client.print("POST " + url + " HTTP/1.1\r\n"
-                                 "Host: " +
-                 host + "\r\n"
-                        "Content-Type: application/json\r\n"
-                        "Content-Length: " +
-                 banaan.length() + "\r\n"
-                                   "\r\n" +
-                 banaan);
-
-    String line = client.readStringUntil('!');
+                  "Host: " + host + "\r\n"
+                  "Content-Type: application/json\r\n"
+                  "Content-Length: " + banaan.length() + "\r\n"
+                  "\r\n" + banaan);
+    
+    String line = client.readStringUntil('z');
     Serial.println(line);
     Serial.println("closing connection");
 
-    int packetSize = LoRa.parsePacket();
-    if (packetSize)
-    {
-      // received a packet
-      Serial.print("Received packet '");
-
-      // read packet
-      while (LoRa.available())
-      {
-        String packetEncrypted = LoRa.readString();
-        mSpiffs.writeFile(SPIFFS, "/encrypted.txt", packetEncrypted);
-        String LoRaData = cipher->decryptString(mSpiffs.getFile(SPIFFS, "/encrypted.txt"));
-        LoRaData = LoRaData.substring(0, LoRaData.indexOf(0x2400));
-        Serial.print(LoRaData);
-
-        for (int i = 0; i < 12; i++)
-        {
-
-          separator = LoRaData.indexOf('|');
-          switch (i)
-          {
-          case 0:
-            id_esp32 = LoRaData.substring(0, separator);
-            break;
-          case 1:
-            id_sensor1 = LoRaData.substring(0, separator);
-            break;
-          case 2:
-            sensor1 = LoRaData.substring(0, separator);
-            break;
-          case 3:
-            id_sensor2 = LoRaData.substring(0, separator);
-            break;
-          case 4:
-            sensor2 = LoRaData.substring(0, separator);
-            break;
-          case 5:
-            sensor3 = LoRaData.substring(0, separator);
-            break;
-          case 6:
-            id_sensor4 = LoRaData.substring(0, separator);
-            break;
-          case 7:
-            sensor4 = LoRaData.substring(0, separator);
-            break;
-          case 8:
-            id_sensor5 = LoRaData.substring(0, separator);
-            break;
-          case 9:
-            sensor5 = LoRaData.substring(0, separator);
-            break;
-          case 10:
-            sensor6 = LoRaData.substring(0, separator);
-            break;
-          case 11:
-            sensor7 = LoRaData.substring(0, separator);
-            break;
-
-          default:
-            break;
-          }
-          LoRaData = LoRaData.substring(separator + 1, LoRaData.length());
-        }
-      }
-
-      // print RSSI of packet
-      Serial.print("' with RSSI ");
-      Serial.println(LoRa.packetRssi());
-      Serial.println();
-      Serial.println(id_esp32);
-      Serial.println(id_sensor1);
-      Serial.println(sensor1);
-      Serial.println(id_sensor2);
-      Serial.println(sensor2);
-      Serial.println(sensor3);
-      Serial.println(id_sensor4);
-      Serial.println(sensor4);
-      Serial.println(id_sensor5);
-      Serial.println(sensor5);
-      Serial.println(sensor6);
-      Serial.println(sensor7);
-    }
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+    Serial.println();
+    Serial.println(id_esp32);
+    Serial.println(id_sensor1);
+    Serial.println(sensor1);
+    Serial.println(id_sensor2);
+    Serial.println(sensor2);
+    Serial.println(sensor3);
+    Serial.println(id_sensor4);
+    Serial.println(sensor4);
+    Serial.println(id_sensor5);
+    Serial.println(sensor5);
+    Serial.println(sensor6);
+    Serial.println(sensor7);
   }
 }
